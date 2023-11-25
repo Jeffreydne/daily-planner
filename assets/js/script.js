ae41f88ed85af34ef54e9a5cf1abd759d9703b0f
Jeffreydne <Jeffreydne@gmail.com>
2023-11-25 09:55:34 -0800

    add initial functions to change color of DOM when needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,37 +19,83 @@ $(document).ready(function () {
 let now = dayjs();
 console.log(now);
 // planner hour to be used in styleHours fxn
-let plannerHour;
+// let plannerHour;
+let hourIndx;
 //functions
 // display today's date in header
 // CONSIDER CHANGING TO DISPLAY 1ST, SND, 3RD, 4TH ETC AS DAY
 function displayToday() {
 currentDday.text(now.format('dddd, MMMM D'));
 }
-
-function styleHours() {
-  let hourNow = Number(now.format('h'));
-  $.each(timeBlockArr, function( i, value) {
-    plannerHour = Number(timeBlockArr[i]['id'].replace('hour-', ''));
-    console.log(plannerHour);
-
-    if(plannerHour < hourNow) {
-
-    } else if(plannerHour > hourNow) {
-
-    } else {
-      
-    }
-  });
-  console.log(hourNow);
+// 3 functions to replace the class past, present or future which will be called if that needs to be changed. A jQuery DOM element is combined with a vanilla JS method because the JS will replace all the members of the class preventing adding classes over previous classes
+function addGreen() {
+  timeBlockArr[0].className = "row time-block future";
 }
+function addRed() {
+  timeBlockArr[4].className = "row time-block present";
+}
+function addGray() {
+  timeBlockArr[6].className = "row time-block past";
+}
+addRed();
+addGreen();
+addGray();
+// function styleHours() {
+//   let hourNow = Number(now.format('H'));
+//     if(hourNow < 9) {
+//       let greenIndexStart = 0;
+//       let greenIndxLimit = 9;
+//     } else if(hourNow > 17) {
+//       let grayIndexStart = 0;
+//       let grayIndexLimit = 9;
+//     } else if (hourNow === 9) {
+//       let redIndex = 0;
+//       // let grayIndexStart = 0;
+//       // let grayIndexLimit = hourNow - 9;
+//       let greenIndexStart = 1;
+//       let greenIndxLimit = 9;
+//     } else if(hourNow === 17) {
+//       let redIndex = 8;
+//       let grayIndexStart = 0;
+//       let grayIndexLimit = 9;
+//     } else (hourNow < 17) {
+//       let redIndex = hourNow - 9;
+//       let grayIndexStart = 0;
+//       let grayIndexLimit = hourNow - 9;
+//       let greenIndexStart = hourNow - 8;
+//       let greenIndxLimit = 9;
+//     } 
+
+
+    // else {
+    //   let redIndex = hourNow + 3;
+    // }
+  // $.each(timeBlockArr, function( i, value) {
+
+    // plannerHour = Number(timeBlockArr[i]['id'].replace('hour-', ''));
+    // console.log(plannerHour, timeBlockArray[i]);
+
+    // if(plannerHour < hourNow) {
+    //   timeBlockArr[i].addClass('past');
+    // } else if(plannerHour > hourNow) {
+    //   timeBlockArr[i].addClass('future');
+    // } else {
+    //   timeBlockArr[i].addClass('present');
+    // }
+
+
+  // });
+
+
+//   console.log(hourNow);
+// }
 //ERASE domQues- used only for confirmation
 console.log(timeBlockArr[0]['id']);
 function domQues() {
 console.log(hour9.val());
 }
 //PROBABLY NEED TO MOVE THIS
-styleHours();
+// styleHours();
 
 //call function to display today
 displayToday();
